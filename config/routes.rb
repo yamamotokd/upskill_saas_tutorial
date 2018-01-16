dffd0a8ea9d86b7010e222598316cdff297af021
Kenji Yamamoto <yamamotokd@gmail.com>
2018-01-16 19:57:28 +0000

    added new pathing for contact-us

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
     root to: 'pages#home'
     get 'about', to: 'pages#about'
     resources :contacts, only: :create
-    get 'contact-us', to: 'contacts#new'
+    get 'contact-us', to: 'contacts#new', as: 'new_contact'
 end
