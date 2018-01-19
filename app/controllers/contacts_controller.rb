e5ef47d524a70e34f9d4f79422061c15c4c9b3ac
Kenji Yamamoto <yamamotokd@gmail.com>
2018-01-19 20:11:21 +0000

    fixed contacts controller file that called wrong parameters from Contact.new

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -6,9 +6,9 @@ class ContactsController < ApplicationController
     def create
         @contact = Contact.new(contact_params)
         if @contact.save
-            name = params[:contacts][:name]
-            email = params[:contacts][:email]
-            body = params[:contacts][:comments]
+            name = params[:contact][:name]
+            email = params[:contact][:email]
+            body = params[:contact][:comments]
             
             ContactMailer.contact_email(name,email,body).deliver
             flash[:success] = "Message sent."
