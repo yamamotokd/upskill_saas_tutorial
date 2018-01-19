ef9dc99e9de59363d67693eede63b8f44d5b3349
Kenji Yamamoto <yamamotokd@gmail.com>
2018-01-19 21:32:27 +0000

    re-migrated contacts table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180119210345) do
+ActiveRecord::Schema.define(version: 20180119212653) do
 
   create_table "contacts", force: :cascade do |t|
     t.string   "name"
