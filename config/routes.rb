ea894a74c5bd217aed0eaf8ed2807eebbe4d4d64
Kenji Yamamoto <yamamotokd@gmail.com>
2018-01-25 18:51:32 +0000

    Changed contact path

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
     root to: 'pages#home'
     get 'about', to: 'pages#about'
     resources :contacts, only: :create
-    get 'contactus', to: 'contacts#new', as: 'new_contact'
+    get 'contactus', to: 'contacts#new', as: 'new_contact_path'
 end
