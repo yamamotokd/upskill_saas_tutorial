e163f5c6a65721d6fe2191adc146f7b3a8571cf3
Kenji Yamamoto <yamamotokd@gmail.com>
2018-01-25 20:33:16 +0000

    added email validation to stop people like me adding adsf emails

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,5 +1,8 @@
 class Contact < ActiveRecord::Base
-    validates :name, presence: true
-    validates :email, presence: true
-    validates :comments, presence: true
+    validates :name, :email, :comments presence: true
+    validates :email, format: { with: /.+@.+\..+/i, on: :create }
+    # https://davidcel.is/posts/stop-validating-email-addresses-with-regex/
+    # david gives a good blog post on why not to overcomplicate the email regex.
+    # its pretty sound specially for my hobby project. I can look into regex more
+    # if I need to, but chances some else is better and gets paid to parse it.
 end
