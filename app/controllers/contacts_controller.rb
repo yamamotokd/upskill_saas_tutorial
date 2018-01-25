56c14b97617ca0918b16b35f7b4810309960f3e5
Kenji Yamamoto <yamamotokd@gmail.com>
2018-01-25 19:43:57 +0000

    changed mailer email

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -11,10 +11,10 @@ class ContactsController < ApplicationController
             body = params[:contact][:comments]
             ContactMailer.contact_email(name, email, body).deliver
             flash[:success] = "Message sent."
-            redirect_to new_contact
+            redirect_to new_contact_path
         else
             flash[:danger] = @contact.errors.full_messages.join(", ")
-            redirect_to new_contact
+            redirect_to new_contact_path
         end
     end
     
