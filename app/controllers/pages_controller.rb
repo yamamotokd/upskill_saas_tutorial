8c0b8547d2f08080d81435dbb66589d5c57d4161
Kenji Yamamoto <yamamotokd@gmail.com>
2018-02-01 23:57:37 +0000

    added links with parameters to variables and commented out the navbar sign up

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,8 @@
 class PagesController < ApplicationController
+    #Get request for / which is our homepage
     def home
+        @basic_plan = Plan.find(1)
+        @pro_plan = Plan.find(2)
     end
     
     def about
