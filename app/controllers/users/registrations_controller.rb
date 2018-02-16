558ff387cca70f09c2242e50de553e4c5b66debc
Kenji Yamamoto <yamamotokd@gmail.com>
2018-02-16 19:56:24 +0000

    added restrictions on plan urls

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,4 +1,6 @@
 class Users::RegistrationsController < Devise::RegistrationsController
+    before_action :select_plan, only: :new
+    
     # Extend defualt devise gem behavior so that users signing up with the pro
     # account (plan id==2) save with a special stripe function.
     # Otherwise Devise saves as a basic user.
@@ -14,4 +16,12 @@ class Users::RegistrationsController < Devise::RegistrationsController
             end
         end
     end
+    
+    private
+        def select_plan
+            unless (params[:plan] == '1' || params[:plan] == '2')
+                flash[:notice] = "Please select a membership plan to sign up."
+                redirect_to root_url
+            end
+        end
 end
