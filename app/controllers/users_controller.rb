ae524edd5b7f918df20435071c2bee51c3184087
Kenji Yamamoto <yamamotokd@gmail.com>
2018-02-25 05:39:01 +0000

    added users action and view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
    before_action :authenticate_user!
-
+    
+    def index
+        
+    end
+    
     def show
         # users/:user_id
         @user = User.find(params[:id])
